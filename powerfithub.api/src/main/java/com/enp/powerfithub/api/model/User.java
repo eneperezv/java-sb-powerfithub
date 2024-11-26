0fc24843c4832ba8cf6f94abee819bfe03d7fc87
eneperezv <eneperezv@gmail.com>
2024-11-26 01:04:16 -0500

    Inclusion de entidades y clases auxiliares para seguridad JWT
     Changes to be committed:
    	modified:   powerfithub.api/pom.xml
    	new file:   powerfithub.api/src/main/java/com/enp/powerfithub/api/model/ErrorDetails.java
    	new file:   powerfithub.api/src/main/java/com/enp/powerfithub/api/model/ResponseDetails.java
    	new file:   powerfithub.api/src/main/java/com/enp/powerfithub/api/model/Token.java
    	new file:   powerfithub.api/src/main/java/com/enp/powerfithub/api/model/User.java
    	new file:   powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/UserRepository.java
    	new file:   powerfithub.api/src/main/java/com/enp/powerfithub/api/service/UserMyDetails.java

---
+++ powerfithub.api/src/main/java/com/enp/powerfithub/api/model/User.java
@@ -0,0 +1,78 @@
+package com.enp.powerfithub.api.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+    private String username;
+    private String password;
+    private String role; //Eg: ADMIN,USER
+    private String name;
+    private LocalDate dateOfBirth;
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + ", name="
+				+ name + ", dateOfBirth=" + dateOfBirth + "]";
+	}
+
+}
