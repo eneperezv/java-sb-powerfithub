64066ad202a7640e92eeb68d670230b03250da5d
eneperezv <eneperezv@gmail.com>
2024-11-28 00:36:13 -0500

    Inclusion de documentacion inicial de las clases
     Changes to be committed:
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/ExerciseRepository.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/UserRepository.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/WorkoutRepository.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/security/JwtAuthenticationFilter.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/security/JwtService.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/security/LoginForm.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/security/WebSecurityConfig.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/service/ExerciseService.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/service/UserMyDetails.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/service/UserService.java
    	modified:   powerfithub.api/src/main/java/com/enp/powerfithub/api/service/WorkoutService.java

--- powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/UserRepository.java
+++ powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/UserRepository.java
@@ -1,11 +1,26 @@
 package com.enp.powerfithub.api.repository;
 
+/*
+ * @(#)UserRepository.java 1.0 27/11/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.enp.powerfithub.api.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 	
 	Optional<User> findByUsername(String username);
--- powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/WorkoutRepository.java
+++ powerfithub.api/src/main/java/com/enp/powerfithub/api/repository/WorkoutRepository.java
@@ -1,9 +1,25 @@
 package com.enp.powerfithub.api.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+/*
+ * @(#)WorkoutRepository.java 1.0 27/11/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
 
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.enp.powerfithub.api.model.Workout;
 
+@Repository
 public interface WorkoutRepository extends JpaRepository<Workout,Long> {
 
 }
